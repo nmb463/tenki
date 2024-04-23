e7d71e54726c3f800f779dd488bd8071d614222c
Nicole Banda <nmbanda124@gmail.com>
2024-04-22 20:43:44 -0700

    Added more weather data to the functionality of the search engine

--- src/app.js
+++ src/app.js
@@ -2,10 +2,34 @@ function updateWeatherData(response) {
     let temperatureElement = document.querySelector("#temperature-value");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#weather-descriptor");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#date-time");
+    let date = new Date(response.data.time * 1000);
 
     cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
+
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}mph`
     temperatureElement.innerHTML = Math.round(temperature);
 }
+
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let day = days[date.getDay()];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day}, ${hours}:${minutes}`
+}
+
 
 function searchCity(city) {
     let apiKey = "f917a08757btf485b3af40o0e41087f1";
@@ -24,4 +48,4 @@ function handleSearchSubmit(event){
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Honolulu");
+searchCity("Portland");
