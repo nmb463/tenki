09475332a04d759b3220554398ab35a173a36b46
Nicole Banda <nmbanda124@gmail.com>
2024-04-22 19:35:30 -0700

    Added search functionality using API

--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
+function updateWeatherData(response) {
+    let temperatureElement = document.querySelector("#temperature-value");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+    let apiKey = "f917a08757btf485b3af40o0e41087f1";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`
+    axios.get(apiUrl).then(updateWeatherData);
+}
+
+
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Honolulu");
